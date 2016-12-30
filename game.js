c6b7a45bd099b766042a13a08a6874588403c955
root <root@ChrisDebian.com>
2016-12-30 17:49:18 +0800

    init

--- game.js
+++ game.js
@@ -1,6 +1,20 @@
-function game(){
-	if(!this.tableGame){
-		this.tableGame = {};
-		console.log(this.tableGame);
-	} 
+function next(){
+	if(!this.player){
+		document.body.appendChild(document.createElement('script')).setAttribute("src", "player.js");
+	}
+	
+	if(!this.player.name){
+		if(document.getElementById("gameInput").value){
+			this.player.name = document.getElementById("gameInput").value;
+		}
+	}
+	
+	if(this.player && this.player.name){
+		this.player.nextTurn();
+	}
+
+	
+	
+	
+	
 }
--- player.js
+++ player.js
@@ -1,3 +1,12 @@
-function player(name) {
-	this.name = name;
+var player = {
+	currentTurn : 0,
+	events : [],
+	nextTurn : function(){
+		this.currentTurn++;
+		var newOutput = document.createElement('div');
+		newOutput.innerHTML=("Name: " + this.name + "<br>Turn: " + this.currentTurn);
+		gameOutput.prepend(newOutput);
+		console.log(this);
+	}
+		
 };
